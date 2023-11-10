20bfbf324eb739d1c743037724f1eb36065bb873
Dinko S <dinko.sulicek@predavaci.algebra.hr>
2023-11-10 21:02:33 +0100

    dodavanje u listu

--- src/Components/ispisnovi.jsx
+++ src/Components/ispisnovi.jsx
@@ -1,14 +1,33 @@
 import React, { useState } from "react";
+import IspisUnosa from "./ispisUnosa";
 
 const Ispisnovi = ({ imena }) => {
   const [names, setNames] = useState(imena);
+  const [firstName, setFirstName] = useState("");
+  const [year, setYear] = useState(0);
   return (
     <div>
-      <ul>
-        {names.map(() => (
-          <li>{}</li>
-        ))}
-      </ul>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="number"
+        value={year}
+        onChange={(e) => setYear(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          setNames([
+            ...names,
+            { id: crypto.randomUUID(), name: firstName, years: year },
+          ]);
+        }}
+      >
+        Dodaj
+      </button>
+      <IspisUnosa names={names} />
     </div>
   );
 };
